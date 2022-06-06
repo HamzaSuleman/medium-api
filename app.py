7ac1e91b59b380057a623f39bccb421d2f855ed4
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-06-06 22:42:28 +0500

    updated serve

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ data = [
 def get_data():
     return jsonify(data)
 
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
