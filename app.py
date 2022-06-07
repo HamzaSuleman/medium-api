eaadb3991040ed770c7a0f5444e52a34b1cbd79f
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-06-07 19:48:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,20 @@ data = [
 
 @app.route('/')
 def index():
-    return jsonify(data)
+    #return jsonify(data)
+    return jsonify([
+  {
+    "userId": 1,
+     "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },])
 
 if __name__ == "__main__":
     app.run()
