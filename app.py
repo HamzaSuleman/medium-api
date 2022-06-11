844d50f10f545fe57871374226e09822264f5b1d
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-06-11 19:53:39 +0500

    updated

--- app.py
+++ app.py
@@ -2,40 +2,23 @@ from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
-orders = {
-  "order1": {
+orders = [
+   {
     "Size": "Small",
     "Toppings":"Cheese",
     "Crust":"Thin Crust",
   },
-  "order2": {
+  {
     "Size": "Large",
     "Toppings":"Cheese",
     "Crust":"Stuffed Crust",
   }
-}
+]
 
 @app.route('/')
 def get():
     response = make_response(jsonify(orders), 200)
     return response
-
-@app.route('/<orderid>')
-def get_order(orderid):
-  if orderid in orders:
-    response = make_response(jsonify(orders[orderid]), 200)
-    return response
-  else:
-    return 'No such data exists'
-
-@app.route('/<orderid>/<items>')
-def get_order_item(orderid, items):
-  item = orders[orderid].get(items)
-  if item:
-    response = make_response(jsonify(item), 200)
-    return response
-  else:
-    return 'No such data exists'
 
 if __name__ == '__main__':
     app.run() 
