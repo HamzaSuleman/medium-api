58ff3c5519d218eee67944d54fd68259c3f64bec
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-06-11 14:26:36 +0500

    updated udemy

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
-import json;
-from flask import Flask, jsonify
+from crypt import methods
+
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 data = [
   {
-    "userId": 1,
-     "id": 1,
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    "id": 1,
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    "userId": 1
   },
   {
-    "userId": 1,
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
     "id": 2,
     "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },]
+    "userId": 1
+  }
+]
 
-@app.route('/')
-def index():
-    #return jsonify(data)
-    return jsonify([
-  {
-    "userId": 1,
-     "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },])
+@app.route('/api', methods=['GET'])
+def get():
+    response = make_response(jsonify(data), 200)
+    return response
 
-if __name__ == "__main__":
-    app.run()
 
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run() 
