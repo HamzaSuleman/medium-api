1b3591046a56099616f13a323ccd3fad86acc2c0
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-07-14 17:50:21 +0500

    Commit Update July

--- app.py
+++ app.py
@@ -1,83 +1,26 @@
 from flask import Flask, jsonify, make_response
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
-orders = [
-   {
-    "Size": "Small",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-  {
-    "Size": "Large",
-    "Toppings":"Cheese",
-    "Crust":"Stuffed Crust",
-  },
-   {
-    "Size": "XL",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "Large",
-    "Toppings":"Mushrooms",
-    "Crust":"Pan Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"pork",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "XXL",
-    "Toppings":"Chicken",
-    "Crust":"Kabab Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
+df = pd.read_csv('friends.csv')
+framelength = len(df.index)
+
+data = []
+
+for i in range(framelength):
+    data.append(
     {
-    "Size": "Large",
-    "Toppings":"Cheese",
-    "Crust":"Stuffed Crust",
-  },
-   {
-    "Size": "XL",
-    "Toppings":"Cheese",
-    "Crust":"Thin Crust",
-  },
-   {
-    "Size": "Large",
-    "Toppings":"Mushrooms",
-    "Crust":"Pan Crust",
-  },
-   {
-    "Size": "Small",
-    "Toppings":"pork",
-    "Crust":"Thin Crust",
-  },
-]
+        'name': df['name'].loc[i],
+        'marks': str(df['marks'].loc[i]),
+        'city': df['city'].loc[i]
+    })
+#print(data)
 
 @app.route('/')
 def get():
-    response = make_response(jsonify(orders), 200)
+    response = make_response(jsonify(data), 200)
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
