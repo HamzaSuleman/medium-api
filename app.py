ca5ca5c7c3b4095c7f72643b9d14b4cea49bec64
HamzaSuleman <84967076+HamzaSuleman@users.noreply.github.com>
2022-07-16 00:13:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 plans = {}
-type = ['Bulking_Plan']
+type = ['Bulking_Plan','Bulking_Plan']
 plans['Paklife '+type[0]] = get_fullplan(type[0])
+plans['Paklife '+type[1]] = get_fullplan(type[1])
 
 @app.route('/')
 def get():
